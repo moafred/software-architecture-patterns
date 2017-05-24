57c662fa5636819b9f19fb8ed15aef44e82ff77b
fred <frederic.oudry@gmail.com>
2017-05-24 10:47:46 +0200

    feat: phase 1

--- config/parameters.php
+++ config/parameters.php
@@ -4,6 +4,7 @@ return [
     'db' => [
         'driver'    => 'pdo_pgsql',
         'host'      => 'postgres',
+        'port'      => '5432',
         'dbname'    => 'postgres',
         'user'      => 'postgres',
         'password'  => 'postgres'
--- src/Controllers/PokemonController.php
+++ src/Controllers/PokemonController.php
@@ -7,11 +7,17 @@ use Doctrine\DBAL\DBALException;
 use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PokemonController
 {
     /** @var Connection */
     private $connection;
+
+    private $pokemons = [
+        'pikachu',
+        'tortank'
+    ];
 
     /**
      * PokemonController constructor.
@@ -73,8 +79,13 @@ class PokemonController
         $type = $request->get('type');
         $level = (int) $request->get('level');
 
-        // TODO check type exists
-        // TODO check level is in bounds
+        if (!in_array($type, $this->pokemons)) {
+            return new JsonResponse('Pokemon not exists', Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($level < 0 || $level >= 30) {
+            return new JsonResponse('Pokemon level not valid', Response::HTTP_BAD_REQUEST);
+        }
 
         $sql = 'INSERT INTO pokemon.collection (uuid, type, level) VALUES (:uuid, :type, :level)';
         $query = $this->connection->prepare($sql);
@@ -97,8 +108,38 @@ class PokemonController
      */
     public function evolve($uuid)
     {
-        // TODO
+        $sql = 'SELECT collection.level FROM pokemon.collection WHERE uuid = :uuid';
+        $query = $this->connection->prepare($sql);
+        $query->bindValue('uuid', $uuid);
+        $query->execute();
 
-        return new JsonResponse([]);
+        $pokemon = $query->fetch();
+
+        if (!$pokemon) {
+            return new JsonResponse('Pokemon does not exists', Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($pokemon['level'] < 7) {
+            return new JsonResponse('Pokemon cannot evoleve', Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($pokemon['level'] >= 15) {
+            return new JsonResponse('Pokemon cannot evoleve', Response::HTTP_BAD_REQUEST);
+        }
+
+        // @todo whoops
+        $newType = 'Raichu';
+
+        $sql = 'UPDATE pokemon.collection SET type = :type WHERE uuid = :uuid';
+        $query = $this->connection->prepare($sql);
+        $query->bindValue('uuid', $uuid);
+        $query->bindValue('type', $newType);
+        $query->execute();
+
+        return new JsonResponse([
+            'uuid' => $uuid,
+            'type' => $newType,
+            'level' => $pokemon['level']
+        ]);
     }
 }
